7b2f890bbf14d192a79975b170ecae755decbba2
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-31 09:37:34 +0300

    New_repo

---
+++ modul1.py
@@ -0,0 +1,13 @@
+import modul2 as m2
+import ConcpectModul as m1
+from modul2 import say_hi as s_h
+# def s_h():
+#     print('Я из модуля 1')
+print(f'привет! я из модуля 1')
+m2.say_hi()
+m1.func()
+m2.say_hi()
+m2.main()
+print('Hi')
+print(m2.__name__)
+s_h()
---
+++ modul2.py
@@ -0,0 +1,12 @@
+def say_hi():
+    print(f'ПРИВЕТ! Я ИЗ МОДУЛЯ 2')
+
+
+def main():
+    a = 5
+    b = 10
+    print(a, b)
+
+
+if __name__ == '__main__' :
+    main()
